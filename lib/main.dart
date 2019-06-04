f31c59ede444f33c1cc10c203df044c74af24561
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-04 15:14:19 +0530

    Initial thing done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Ask Me Anything"),
+            backgroundColor: Colors.indigo,
+            centerTitle: true,
+          ),
+          backgroundColor: Colors.lightBlue,
+          body: BallApp(),
+        )
       ),
     );
+
+class BallApp extends StatefulWidget {
+  @override
+  _BallAppState createState() => _BallAppState();
+}
+
+class _BallAppState extends State<BallApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
