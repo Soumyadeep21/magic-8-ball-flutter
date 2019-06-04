a87bf5b73c4fd64330e77b61b23337f0c3fc5357
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-04 15:23:31 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Ask Me Anything"),
-            backgroundColor: Colors.indigo,
-            centerTitle: true,
-          ),
-          backgroundColor: Colors.lightBlue,
-          body: BallApp(),
-        )
-      ),
+          home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ask Me Anything"),
+          backgroundColor: Colors.indigo,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.lightBlue,
+        body: BallApp(),
+      )),
     );
 
 class BallApp extends StatefulWidget {
@@ -20,10 +21,20 @@ class BallApp extends StatefulWidget {
 }
 
 class _BallAppState extends State<BallApp> {
+
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset("images/ball1.png"),
+      child: FlatButton(
+        child: Image.asset("images/ball$ballNumber.png"),
+        onPressed: (){
+          setState(() {
+           ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+      ),
     );
   }
 }
